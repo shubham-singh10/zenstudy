dfc120f6dc200cce586938663c8e906850891ef6
Shubham Kumar Singh <shubhamsinghh9599@gmail.com>
2024-08-02 13:59:35 +0530

    Changes

--- src/studentDashboard/Sidebar.jsx
+++ src/studentDashboard/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { FiLogOut, FiUser, FiBook, FiAward, FiChevronRight, FiCircle, FiChevronLeft } from 'react-icons/fi';
+import { FiLogOut, FiUser, FiBook, FiCircle, FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import Cookies from 'js-cookie';
 
 const links = [
@@ -19,41 +19,28 @@ const links = [
         link: "/mycourse",
         icon: <FiCircle />,
     },
-
-    // {
-    //     label: "UPSC",
-    //     link: "/upsc-student",
-    //     icon: <FiAward />,
-    // },
 ];
 
 const Sidebar = () => {
     const [isCollapsed, setIsCollapsed] = useState(false);
     const location = useLocation();
-    const navigate = useNavigate()
     const toggleSidebar = () => {
         setIsCollapsed(!isCollapsed);
     };
 
-    const handleLogout =()=>{
-        Cookies.remove('access_tokennew')
-        localStorage.removeItem("userData")
-        navigate("/")
-    }
 
     return (
-        <div className={`hidden h-full lg:flex flex-col justify-between py-8 shadow-xl ${isCollapsed ? 'w-20' : 'w-1/6'} transition-all duration-300`}>
+        <div className={`hidden h-full lg:flex flex-col py-8 shadow-xl transition-all duration-300 ${isCollapsed ? 'w-20' : 'w-64'}`}>
             <div className="flex items-center justify-between px-4">
-                <div className="flex items-center">
-                    <Link to={"/"} className={`text-3xl font-bold ${isCollapsed ? 'hidden' : 'block'}`}>
-                        ZenStudy<span className="text-[#054BB4] text-4xl">.</span>
-                    </Link>
-                </div>
-                <button onClick={toggleSidebar} className=" block text-2xl focus:outline-none">
-                    {isCollapsed ? <FiChevronRight /> : <FiChevronLeft />}
+                <img src={"/assets/logo.png"} alt="Logo" className={`h-10 w-10 ${isCollapsed ? 'block' : 'hidden'}`} />
+                <Link to={"/"} className={`text-3xl font-bold ${isCollapsed ? 'hidden' : 'block'}`}>
+                    ZenStudy<span className="text-[#054BB4] text-4xl">.</span>
+                </Link>
+                <button onClick={toggleSidebar} className="text-2xl focus:outline-none">
+                    {isCollapsed ? <FiChevronRight size={35} /> : <FiChevronLeft size={35} />}
                 </button>
             </div>
-            <div className={`flex flex-col items-center justify-center gap-4 font-medium text-xl ${isCollapsed ? 'mt-8' : 'mt-16'}`}>
+            <div className={`flex flex-col mt-36 items-center justify-center gap-4 font-medium text-xl ${isCollapsed ? 'mt-8' : 'mt-16'}`}>
                 {links.map(({ label, link, icon }) => (
                     <Link to={link} key={label} className={`w-full flex items-center py-3 px-4 ${location.pathname === link ? 'bg-[#054BB4] text-white' : 'text-black hover:bg-gray-200'} transition-colors duration-200`}>
                         <div className="flex items-center gap-4">
@@ -63,12 +50,7 @@ const Sidebar = () => {
                     </Link>
                 ))}
             </div>
-            <div>
-                <button className="bg-red-500 w-full py-3 text-lg text-white font-semibold flex items-center justify-center" onClick={()=> handleLogout()}>
-                    <FiLogOut className="text-2xl mr-2" />
-                    {!isCollapsed && 'Log Out'}
-                </button>
-            </div>
+
         </div>
     );
 };
--- src/studentDashboard/StudentDashNavbar.jsx
+++ src/studentDashboard/StudentDashNavbar.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { IoClose, IoNotifications } from "react-icons/io5";
 import { RxHamburgerMenu } from "react-icons/rx";
-import { FiUser, FiBook, FiAward, FiCircle } from "react-icons/fi";
+import { FiUser, FiBook, FiCircle, FiLogOut } from "react-icons/fi";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
+
 const links = [
   {
     label: "Profile",
@@ -20,20 +21,16 @@ const links = [
     link: "/mycourse",
     icon: <FiCircle />,
   },
-  // {
-  //     label: "UPSC",
-  //     link: "/upsc-student",
-  //     icon: <FiAward />,
-  // },
 ];
-
 
 const StudentDashNavbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [userData, setUserData] = useState(null);
   const location = useLocation();
   const navigate = useNavigate();
-
+  const dropdownRef = useRef(null);
+  const sidebarRef = useRef(null);
 
   useEffect(() => {
     const userDataFromLocalStorage = localStorage.getItem("userData");
@@ -42,57 +39,85 @@ const StudentDashNavbar = () => {
       setUserData(parsedUserData);
     }
   }, []);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [sidebarRef]);
+
   const handleClick = () => {
     setIsOpen(false);
   };
 
-
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   const handleLogout = () => {
     localStorage.removeItem("userData");
     Cookies.remove("access_tokennew");
     navigate("/");
     setIsOpen(!isOpen);
   };
+
+  const closeDropdown = () => {
+    setIsDropdownOpen(false);
+  };
+
   return (
     <div className="m-5 h-[10vh] shadow-lg flex items-center justify-between px-4 lg:px-12 relative bg-white">
       <div>
-        <p className="font-semibold text-lg hidden lg:block">
-          Student Dashboard
-        </p>
+        {/* <p className="font-semibold text-lg hidden lg:block">Student Dashboard</p> */}
         <RxHamburgerMenu
           className="lg:hidden block text-2xl cursor-pointer"
           onClick={toggleSidebar}
         />
         {isOpen && (
-          <div className="h-screen md:w-1/4 lg:hidden w-full bg-white shadow-lg absolute top-0 left-0 z-50">
-            <IoClose
-              className="text-3xl absolute right-3 top-2"
-              onClick={toggleSidebar}
-            />
-            <div className="flex items-center justify-between flex-col h-full py-11">
+          <div ref={sidebarRef} className="h-screen md:w-1/4 lg:hidden w-full bg-white shadow-lg absolute top-0 left-0 z-50">
+            <div className="flex items-center flex-col h-full py-11">
               <div className="flex items-center flex-col w-full px-12">
                 <Link to={"/"} className="text-3xl font-bold">
-                  ZenStudy<span className="text-[#054BB4] text-4xl">.</span>
+                  <img src={"/assets/logo.png"} alt="Logo" className={`h-90 w-90`} />
                 </Link>
-                <p className="text-[10px] font-medium text-[#054BB4]">
-                  Making Education Imaginative
-                </p>
+                <IoClose
+                  className="text-3xl absolute right-3 top-2"
+                  onClick={toggleSidebar}
+                />
               </div>
-              <div
-                className={`flex flex-col items-center justify-center gap-4 font-medium text-lg`}
-              >
+              <div className={`flex flex-col items-center justify-center gap-4 mt-8 font-medium text-lg`}>
                 {links.map(({ label, link, icon }) => (
                   <Link
                     to={link}
                     key={label}
-                    className={`w-full flex items-center py-3 px-12 ${
-                      location.pathname === link
-                        ? "bg-[#054BB4] text-white"
-                        : "text-black hover:bg-gray-200"
-                    } transition-colors duration-200`}
+                    className={`w-full flex items-center py-3 px-12 ${location.pathname === link
+                      ? "bg-[#054BB4] text-white"
+                      : "text-black hover:bg-gray-200"
+                      } transition-colors duration-200`}
                     onClick={handleClick}
                   >
                     <div className="flex items-center gap-4">
@@ -102,35 +127,52 @@ const StudentDashNavbar = () => {
                   </Link>
                 ))}
               </div>
-              <button
-                className="bg-red-500 w-full py-3 text-lg text-white font-semibold"
-                onClick={() => handleLogout()}
-              >
-                Log Out
-              </button>
             </div>
           </div>
         )}
       </div>
       <div className="flex items-center justify-between gap-7">
-        <IoNotifications className="lg:text-2xl md:text-xl text-lg text-[#054BB4]" />
-        <div className="flex items-center gap-4">
-          <img
-            src={userData?.avatar || "https://i.ibb.co/GcKk9fh/images-2.jpg"}
-            alt="Profile"
-            className="lg:w-14 md:w-12 w-10 rounded-full"
-          />
-          <div className="flex flex-col items-end">
-            <p className="text-[#054BB4] font-semibold sm:text-sm md:text-lg lg:text-lg">
-              {userData?.name}
-            </p>
-            <p className="text-[#CADEFB] text-sm">Student</p>
+        {/* <IoNotifications className="lg:text-2xl md:text-xl text-lg text-[#054BB4]" /> */}
+        <div className="relative">
+          <div className="flex items-center gap-4 cursor-pointer" onClick={toggleDropdown}>
+            <div className="flex flex-col items-end">
+              <p className="text-[#054BB4] font-semibold sm:text-sm md:text-lg lg:text-lg">
+                {userData?.name}
+              </p>
+              <p className="text-[#CADEFB] text-sm">Student</p>
+            </div>
+            <img
+              src={userData?.avatar || "https://i.ibb.co/GcKk9fh/images-2.jpg"}
+              alt="Profile"
+              className="lg:w-14 md:w-12 w-10 rounded-full"
+            />
           </div>
+          {isDropdownOpen && (
+            <div
+              ref={dropdownRef}
+              className={`absolute right-0 mt-4 w-48 bg-white border border-gray-300 rounded-lg shadow-lg z-50 transition-transform transform ${isDropdownOpen ? "scale-100" : "scale-0"
+                }`}
+            >
+              <Link
+                to="/profile"
+                className="flex items-center px-4 py-2 mt-2 hover:bg-gray-200"
+                onClick={closeDropdown}
+              >
+                <FiUser className="mr-2" /> Profile
+              </Link>
+              <hr className="border-t-2 border-gray-500 my-4" />
+              <button
+                className="ml-6 mb-2 mt-2 flex items-center px-8 py-2 bg-red-500 text-white hover:bg-red-700 font-semibold rounded transition-colors duration-200"
+                onClick={handleLogout}
+              >
+                <FiLogOut className="mr-2" /> Logout
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
   );
 };
-
 
 export default StudentDashNavbar;
