f73514d8dcafa9f81eb3e149d68668d86e1791b8
Harsh <harshkhandewal2001@gmail.com>
2024-09-23 16:52:39 +0530

    last change

--- src/components/course/Coursedetails.jsx
+++ src/components/course/Coursedetails.jsx
@@ -270,7 +270,7 @@ const CourseDetailsView = () => {
       <div className="p-4 lg:p-12 bg-blue-100 w-full md:p-8 rounded-md flex flex-col justify-start items-start">
         <button
           className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-4 flex items-center lg:-mt-10 md:-mt-6 sm:mt-0"
-          onClick={() => navigate(-1)}
+          onClick={() => navigate('/courses')}
         >
           <FiArrowLeft className="w-5 h-5 mr-2" />
           Back
