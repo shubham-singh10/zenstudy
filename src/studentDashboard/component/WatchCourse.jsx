c05979923022c4bc73d5f938e2a921fdecd21b03
Shubham Kumar Singh <shubhamsinghh9599@gmail.com>
2024-10-28 16:36:46 +0530

    some

--- src/studentDashboard/component/WatchCourse.jsx
+++ src/studentDashboard/component/WatchCourse.jsx
@@ -3,8 +3,6 @@ import { IoIosArrowBack } from "react-icons/io";
 import { useNavigate, useParams } from "react-router-dom";
 import { ZoomMtg } from "@zoom/meetingsdk";
 import Cookies from "js-cookie";
-import he from "he";
-
 
 // ZoomMtg.preLoadWasm();
 // ZoomMtg.prepareWebSDK();
@@ -18,9 +16,11 @@ const WatchCourse = () => {
   const [selectedVideoDesc, setSelectedVideoDesc] = useState(null);
   const { id } = useParams();
   const navigate = useNavigate();
-  const tabs = ["About Video"];
+  const tabs = ["About Video"
+    // , "Q&A", "Reviews"
+    ];
   const [isMeetingStarted, setIsMeetingStarted] = useState(false);
-  const [meetingId, setmeetingId] = useState(null);
+  const [meetingId, setmeetingId] = useState(null)
   const [meetloading, setMeetLoading] = useState(false);
   const token = Cookies.get("access_tokennew");
 
@@ -35,25 +35,31 @@ const WatchCourse = () => {
               Accept: "application/json",
               "Content-Type": "application/json",
             },
-            body: JSON.stringify({ id }),
+            body: JSON.stringify({ id }),  // id from useParams
           }
         );
 
         if (response.status === 204) {
+          // Redirect if no courses found
           navigate("/mycourse");
           return;
         }
 
         if (!response.ok) {
+          // Handle network errors or invalid response
           throw new Error("Network response was not ok");
         }
 
         const data = await response.json();
+        // console.log("MyCourse_purchase", data.response);
+
         setCourses(data.response?.modules);
         setmeetingId(data.response.course.meetingId);
         setLoading(false);
+
       } catch (error) {
         console.error("Error:", error);
+        // Redirect to mycourse on error
         navigate("/mycourse");
       }
     };
@@ -61,6 +67,7 @@ const WatchCourse = () => {
     myCourse();
   }, [id, navigate]);
 
+  //************************************ Meeting Function Start ***************************************************************//
   const onSubmit = async (id) => {
     setMeetLoading(true);
     try {
@@ -91,6 +98,7 @@ const WatchCourse = () => {
       if (data.meetingData) {
         let jdata = data.meetingData;
 
+        // Add participant to the meeting
         const participantResponse = await fetch(
           `${process.env.REACT_APP_API}zenstudy/api/meeting/addParticipant`,
           {
@@ -133,13 +141,20 @@ const WatchCourse = () => {
   const startMeeting = (signature, meetingNumber, password, username) => {
     document.getElementById("zmmtg-root").style.display = "block";
 
+    // console.log("Starting meeting with details:", {
+    //   signature,
+    //   meetingNumber,
+    //   password,
+    //   userName: username,
+    // });
+
     ZoomMtg.init({
       leaveUrl: "https://zenstudy.in/",
       patchJsMedia: true,
       leaveOnPageUnload: true,
       success: (success) => {
         console.log(success);
-        setIsMeetingStarted(true);
+        setIsMeetingStarted(true)
         ZoomMtg.join({
           signature: signature,
           sdkKey: process.env.REACT_APP_ZOOM_SDK_KEY,
@@ -151,6 +166,7 @@ const WatchCourse = () => {
           zak: zakToken,
           success: (success) => {
             console.log(success);
+
           },
           error: (error) => {
             console.log(error);
@@ -162,6 +178,8 @@ const WatchCourse = () => {
       },
     });
   };
+
+  //************************************ Meeting Function End   ***************************************************************//
 
   useEffect(() => {
     if (courses.length > 0) {
@@ -192,32 +210,34 @@ const WatchCourse = () => {
     setSelectedVideoDesc(videoDesc);
   };
 
-
   return (
-    <div className="container mx-auto px-4 py-6">
+    <div className="container mx-auto p-4">
       <button
-        className="px-4 py-2 bg-blue-600 rounded-full mb-4 text-white flex items-center gap-2"
+        className="px-6 py-2 bg-blue-600 rounded-full mb-4 text-white flex justify-center items-center gap-2"
         onClick={() => navigate("/mycourse")}
       >
         <IoIosArrowBack /> Back
       </button>
       <div className="flex flex-col lg:flex-row lg:space-x-8">
-        <div className="w-full lg:w-2/3">
-          <iframe
-            src={`${url}`}
-            frameBorder="0"
-            className="h-[40vh] lg:h-[70vh] w-full"
-            allow="autoplay; fullscreen; picture-in-picture"
-            title="zenstudy"
-          ></iframe>
+        {/* Video Section */}
+        <div className="lg:w-2/3">
+          <div>
+            <iframe
+              src={`${url}`}
+              frameBorder="0"
+              className="top-0 left-0 h-[70vh] w-[100%]"
+              allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
+              title="zenstudy"
+            ></iframe>
+          </div>
           <div className="mt-4">
-            <div className="flex space-x-4 border-b">
+            <div className="flex space-x-8 border-b">
               {tabs.map((tab) => (
                 <button
                   key={tab}
                   className={`text-lg p-2 ${selectedTab === tab
-                      ? "border-b-2 border-blue-500 text-blue-500"
-                      : "text-gray-500"
+                    ? "border-b-2 border-blue-500 text-blue-500"
+                    : "text-gray-500"
                     }`}
                   onClick={() => setSelectedTab(tab)}
                 >
@@ -228,13 +248,16 @@ const WatchCourse = () => {
             <div className="mt-4">
               {selectedTab === "About Video" && (
                 <div>
-                  <p className="text-gray-700" >{selectedVideoDesc}</p>
+                  <p className="text-gray-700">{selectedVideoDesc}</p>
                 </div>
               )}
+              {selectedTab === "Q&A" && <div>Q&A Content</div>}
+              {selectedTab === "Reviews" && <div>Reviews Content</div>}
             </div>
           </div>
         </div>
-        <div className="w-full lg:w-1/3 mt-8 lg:mt-0">
+        {/* Course Content Section */}
+        <div className="lg:w-1/3 mt-8 lg:mt-0">
           <h2 className="text-2xl font-bold mb-4">Course Content</h2>
           {courses.length > 0 &&
             courses.map((module, index) => (
@@ -257,8 +280,8 @@ const WatchCourse = () => {
                     <div
                       key={index}
                       className={`flex items-center p-2 border-t ${selectedVideoTitle === video.videoTitle
-                          ? "text-blue-500"
-                          : ""
+                        ? "text-blue-500"
+                        : ""
                         }`}
                       onClick={() =>
                         handleVideoClick(
@@ -279,20 +302,51 @@ const WatchCourse = () => {
         </div>
       </div>
 
-      {meetingId && (
-        <div className="mt-8">
+      {/* Meeting join button */}
+      {!isMeetingStarted && meetingId && (
+        <button
+          onClick={() => onSubmit(meetingId)}
+          disabled={meetloading}
+          className={`flex justify-end ${meetloading
+              ? "bg-red-500 hover:bg-red-700"
+              : "bg-blue-600 hover:bg-blue-700 animate-glow"
+            } text-white z-50 rounded-full fixed bottom-0 right-10 mb-6 text-xl py-2 px-8`}
+        >
           {meetloading ? (
-            <div className="text-center text-xl">Joining...</div>
+            <Fragment>
+              <svg
+                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8v8h8a8 8 0 11-16 0z"
+                ></path>
+              </svg>
+              Please wait...
+            </Fragment>
           ) : (
-            <button
-              className="px-6 py-3 bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-600"
-              onClick={() => onSubmit(meetingId)}
-            >
-              Join Zoom Meeting
-            </button>
+            "Join Live"
           )}
-        </div>
+        </button>
       )}
+
+
+      <div id="zmmtg-root">
+        {/* Zoom Meeting SDK Component View Rendered Here */}
+      </div>
+      {/* Meeting join button */}
     </div>
   );
 };
