573b69f97f1df7e51121326df00dae96e640850e
Shubham Kumar Singh <shubhamsinghh9599@gmail.com>
2024-12-13 15:54:48 +0530

    some changes

--- src/components/PdfViewer.jsx
+++ src/components/PdfViewer.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import { Worker, Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+
+// Import styles
 import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 const PdfViewer = () => {
+    const plugin = defaultLayoutPlugin()
     return (
-        <div className="mt-8 bg-gray-100 p-4 border rounded-lg">
-            <h2 className="text-xl font-bold text-gray-800 mb-4">PDF Viewer</h2>
-            <div style={{ height: "100vh" }}>
+        <div
+        style={{
+            border: '1px solid rgba(0, 0, 0, .3)',
+            display: 'flex',
+            height: '50rem',
+            margin: '1rem auto',
+            width: '64rem',
+        }}
+    >
                 <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-                    <Viewer fileUrl="https://pdfobject.com/pdf/sample.pdf" />
+                    <Viewer fileUrl="https://pdfobject.com/pdf/sample.pdf" plugin={[plugin]}/>
 
-                    ...
                 </Worker>
-            </div>
-        </div>
+                </div>
+           
     )
 }
 
