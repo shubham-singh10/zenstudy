7ad8a27cc4331de2c3db4c8ac9a65f9207dc66ac
Shubham Kumar Singh <shubhamsinghh9599@gmail.com>
2025-04-24 16:27:40 +0530

    some changes

--- src/usePageTracking.jsx
+++ src/usePageTracking.jsx
@@ -3,22 +3,12 @@ import { useLocation } from 'react-router-dom';
 
 export const usePageTracking = () => {
     const location = useLocation();
-
     useEffect(() => {
         // Google Analytics
-        if (window.gtag) {
-            window.gtag('config', 'G-Y5L03F2HBX', {
-                page_path: location.pathname,
+        if (location.pathname === '/webinar') {
+            window.gtag('event', 'ads_conversion_Sign_up_Page_load_zenst_1', {
+              // <event_parameters>
             });
-
-            window.gtag('event', 'page_view', {
-                page_path: location.pathname,
-            });
-        }
-
-        // Meta (Facebook) Pixel
-        if (window.fbq) {
-            window.fbq('track', 'PageView');
-        }
+          }
     }, [location]);
 };
